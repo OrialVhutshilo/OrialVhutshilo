0a2859192eaeac61c7b33dfa7ec13bb609197f8c
OrialVhutshilo <166949856+OrialVhutshilo@users.noreply.github.com>
2024-04-17 10:07:17 +0200

    Add files via upload

---
+++ my.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+    <title>
+        register
+    </title>
+</head>
+<body>
+    <form action="welcome.php" method="post">
+        Name: <br/><input type="text" name="name" /><br />
+        phone no: <br/><input type="text" name="phone" /><br />
+        <input type="submit" value="submit">
+    </form>
+</body>
+
+
+</html>
